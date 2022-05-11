328d9f6d71fb40ac2dde3cfe581c47a92efdff5b
Erathia Xie <Ecytchia@gmail.com>
2022-05-11 15:47:31 +0800

    dragging / on delete / on create cards

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,21 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 import data from "./data.json";
-import Board from 'react-trello'
+import Board from 'react-trello';
+import Modal from 'react-bootstrap/Modal';
+import ModalDialog from 'react-bootstrap/ModalDialog';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import Button from 'react-bootstrap/Button';
+
+const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
+  console.log('drag ended')
+  console.log(`cardId: ${cardId}`)
+  console.log(`sourceLaneId: ${sourceLaneId}`)
+  console.log(`targetLaneId: ${targetLaneId}`)
+}
+
+const handleCardAdd = (card, laneId) => {
+  console.log(`New card added to lane ${laneId}`)
+  console.dir(card)
+}
+
+const handleCardDelete = (cardId) => {
+  console.log(`Card has been deleted ${cardId}`)
+}
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <h1>Your Work Progression</h1>
+
       <Board
         data={data}
+        onCardAdd={handleCardAdd}
+        onCardDelete={handleCardDelete}
+        handleDragEnd={handleDragEnd}
         draggable
         editable
-        canAddLanes
-        addLaneTitle="Add Column"
         addCardTitle="Add Item"
       />
-    </div>
+
+    </div >
   );
 }
 
