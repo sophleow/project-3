de1ed7c518f2c6735ae0c5db273a7499119605bf
sophleow <leowsophie@gmail.com>
2022-05-13 09:59:21 +0800

    deleted all files

--- back-end/routes/register.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UserDataController = require("../controllers/user.controller");
-const userdatacontroller = new UserDataController();
-
-router.post("/register", userdatacontroller.register);
-
-module.exports = router;
