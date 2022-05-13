ec9c6e67bf039f23d92991c2703c5655e2af995a
sophleow <leowsophie@gmail.com>
2022-05-13 11:39:22 +0800

    add db and index.js

---
+++ back-end/db.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(
+	'project3',
+	'postgres',
+	`${process.env.POSTGRES_PASSWORD}`,
+	{
+		host: 'localhost',
+		dialect: 'postgres',
+	}
+);
+
+const testConnection = async () => {
+	try {
+		await sequelize.authenticate();
+		return true;
+	} catch (err) {
+		console.log(`Couldn't connect to the database`, err);
+		return false;
+	}
+};
+
+module.exports = {
+	testConnection,
+	sequelize,
+};
---
+++ back-end/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('./routes');
+const { sequelize } = require('./db');
+
+const PORT = process.env.PORT || 3001;
+// (async () => {
+// 	await sequelize.sync({ force: true });
+// })();
+
+app.listen(PORT, () => {
+	console.log(`Listening to port ${PORT} ...`);
+});
